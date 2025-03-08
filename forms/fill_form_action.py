388205e52043509a93afbc4fa68fc4bd55dc092a
shizhongqi <shishaoqi0928@gmail.com>
2025-03-08 16:29:42 +0800

    还原下拉框操作

--- forms/fill_form_action.py
+++ forms/fill_form_action.py
@@ -68,12 +68,12 @@ class Fill_action:
 
         state_idx =  [ 0,        1,   2,    3,     4,   5,     6,    7,    8,    9,   10,   11,   12,   13,   14,   15,    16,  17,   18,   19,     20,    21,   22,  23,   24,    25,   26,   27,   28,   29,   30,   31,   32,  33,   34,   35,   36,    37,  38,   39,   40,   41,    42,    43,    44,   45,   46,  47,    48,   49,   50,   51,   52,   53,   54,   55,   56,   57,   58,   59]
         if form_ele == "address_state":
-            # for key in range(59):
-            #     pyautogui.press('up')
-            self._click_element_by_shiftUp(100)
-            for i in range(4):
-                self.mouse_controller.scroll_up(185)
-                time.sleep(0.26)
+            for key in range(59):
+                pyautogui.press('up')
+            # self._click_element_by_shiftUp(100)
+            # for i in range(4):
+            #     self.mouse_controller.scroll_up(185)
+            #     time.sleep(0.26)
             idx = 0
             for i, s in enumerate(state_list):
                 if s == val:
